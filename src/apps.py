3cf3dda628bcaf5ad133becbb0518acaa43e19b0
Will Gearty <willgearty@gmail.com>
2021-09-10 09:35:22 -0500

    Fix importing of caches for python 3

--- src/apps.py
+++ src/apps.py
@@ -39,7 +39,7 @@ class ArgCacheConfig(AppConfig):
             try:
                 importlib.import_module('.caches', app_config.name)
             except ImportError as e:
-                if e.args == ('No module named caches',):
+                if e.args == ('No module named caches',) or e.args == ("No module named '" + app_config.name + ".caches'",):
                     continue
                 raise
 
