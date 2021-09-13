383933c86873f62653c373f6348685464f5a21cf
Will Gearty <willgearty@gmail.com>
2021-09-13 10:57:38 -0500

    Conditionally decode marinade_dish return

--- src/marinade.py
+++ src/marinade.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
+import six
 __author__    = "Individual contributors (see AUTHORS file)"
 __date__      = "$DATE$"
 __rev__       = "$REV$"
@@ -58,7 +59,7 @@ def marinade_dish(arg):
     if isinstance(arg, QuerySet):
         return marinade_dish(list(arg))
     if isinstance(arg, list):
-        return '[%s]' % ','.join([marinade_dish(item).decode('UTF-8') for item in arg])
+        return '[%s]' % ','.join([marinade_dish(item).decode('UTF-8') if isinstance(marinade_dish(item), six.binary_type) else marinade_dish(item) for item in arg])
     if isinstance(arg, Model):
         if arg.id is None:
             import random
