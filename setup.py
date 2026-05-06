7a21c43dcb44bb753725d605dcfdd83e9d375df2
William Gearty <willgearty@gmail.com>
2026-05-05 23:26:16 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-argcache',
-    version='0.2.3',
+    version='0.3',
     license='AGPLv3',
     description='A function-level caching and invalidaton framework for Django.',
     long_description=README,
