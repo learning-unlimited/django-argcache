75f9b6ba0e377f95b60f0c8aaa448f0c7d4860f8
William Gearty <willgearty@gmail.com>
2026-05-05 20:13:17 -0400

    Fix deconstruct and inheritance

--- src/extras/derivedfield.py
+++ src/extras/derivedfield.py
@@ -93,15 +93,12 @@ def DerivedField(FieldCls, getter_fn):
 
     class NewCls(FieldCls):
         """ Wrapper class for the %s model-field, giving a field that is automatically updated """ % FieldCls.__name__
-        def __init__(self, *args, **kwargs):
-            super(NewCls, self).__init__(*args, **kwargs)
-
-            ## Lock to prevent reentrancy on our handler function
-            self._derived_reentrant_lock = False
-            self._handler_registered = False
+        ## Lock to prevent reentrancy on our handler function
+        _derived_reentrant_lock = False
+        _handler_registered = False
 
         def contribute_to_class(self, cls, name, **kwargs):
-            super(NewCls, self).contribute_to_class(cls, name, **kwargs)
+            super().contribute_to_class(cls, name, **kwargs)
             
             # Register the handler after the field is attached to the model
             if not self._handler_registered:
@@ -123,8 +120,8 @@ def DerivedField(FieldCls, getter_fn):
                                     elt.save()
                     
                         else:
-                            item = list(key_set.values())[0]
                             ## TODO: The following test doesn't actually work; self.model claims to be an instance of ModelBase, even though we seem to be able to query it
+                            #item = list(key_set.values())[0]
                             #assert type(item) == type(self.model), "Error, passed an item of type %s into a cache-function that only accepts items of type %s" % (str(type(item)), str(type(self.model)))
                             row = self.model.objects.get(id=list(key_set.values())[0].id)  ## Get a new instance of this model, so that we don't have to worry about unsaved data in other fields
                             new_val = getter_fn(row)
@@ -139,8 +136,12 @@ def DerivedField(FieldCls, getter_fn):
 
         # Make Django think we're in the FieldCls for the purpose of migrations
         def deconstruct(self):
-            name, path, args, kwargs = super(NewCls, self).deconstruct()
-            path = "%s.%s" % (FieldCls.__module__, FieldCls.__name__)
-            return name, path, args, kwargs
+            name, path, args, kwargs = super().deconstruct()
+            # Use the shortest valid import path, matching what Django writes in migrations
+            import django.db.models as models_module
+            if hasattr(models_module, FieldCls.__name__):
+                path = "django.db.models.%s" % FieldCls.__name__
+            else:
+                path = "%s.%s" % (FieldCls.__module__, FieldCls.__qualname__)
 
     return NewCls
